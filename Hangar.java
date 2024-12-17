2d9912d8329f9b5f97a7f6f9ebac1cdccf03eecd
matthieu <matthieu@wegrow-app.com>
2024-12-17 15:35:12 +0100

    Quest 338 - Vehicles

---
+++ Hangar.java
@@ -0,0 +1,12 @@
+public class Hangar {
+
+  public static void main(String[] args) {
+
+    Car ouioui = new Car("Ford", 100);
+    Boat titanic = new Boat("Titanic", 12648);
+
+    System.out.println(ouioui.doStuff());
+    System.out.println(titanic.doStuff());
+  }
+
+}
